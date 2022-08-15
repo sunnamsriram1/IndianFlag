9f58b922696107e25a953f1174fc459942147324
Sunnamsriram <59051820+sunnamsriram1@users.noreply.github.com>
2022-08-15 21:38:11 +0530

    Add files via upload

---
+++ IndiaFlag.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 18:19:43 2022
+
+@author: sriram
+"""
+
+from sketchpy import library as Sriram
+obj = Sriram.flag()
+obj.draw()
